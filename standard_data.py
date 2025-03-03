21768598cc3d096cd5fd7ffd551c527446f81fb9
Debbah Adam <adam.debbah@student-cs.fr>
2025-03-03 17:04:46 +0100

    standardized farmsize data

--- standard_data.py
+++ standard_data.py
@@ -1,36 +1,22 @@
 import pandas as pd
-import os
+import numpy as np
 
-# Charger le fichier CSV dans un DataFrame
+# Charger le fichier CSV en spécifiant le bon séparateur (point-virgule)
 file_path = "Pressure data/FARMSIZE.csv"
 df = pd.read_csv(file_path, sep=";", decimal=",", index_col=0)
 
-# Afficher les premières lignes pour vérifier
-print(df.head())
 
-# Calculer la différence relative entre les colonnes (en commençant par C)
-for i in range(2, len(df.columns)-1, 2):  # Commence à l'indice 2 (colonne C)
-    col1 = df.columns[i]     # Par exemple 'C'
-    col2 = df.columns[i+1]   # Par exemple 'E'
-    
-    # Calcul de la différence relative et ajout dans une nouvelle colonne
-    new_col_name = f'{col2}_{col1}_rendements'  # Nouveau nom pour la colonne calculée
-    df[new_col_name] = (df[col2] - df[col1]) / df[col1]
 
-# Afficher le DataFrame avec les nouvelles colonnes pour vérifier
-print(df.head())
+df = df.apply(pd.to_numeric, errors='coerce')
 
-# Afficher le répertoire actuel
-print("Répertoire actuel avant changement :", os.getcwd())
+# Calcul du rendement pour chaque pays en suivant la formule donnée
+# Utilisation de shift(1) pour obtenir les valeurs de la colonne suivante (t+1)
+df_normalized = (df.shift(-1) - df) / df
 
-# Changer le répertoire de travail
-os.chdir('/Users/r/Documents/projetS6/ai_birds_analysis/Pressure data')
+# Remplacement des valeurs extrêmes par NaN (par exemple, valeurs infinies ou NaN obtenues dans le calcul)
+df_normalized = df_normalized.replace([np.inf, -np.inf], np.nan)
 
-# Vérifier que le répertoire a bien été changé
-print("Répertoire actuel après changement :", os.getcwd())
 
-# Sauvegarder les résultats dans un nouveau fichier CSV
-df.to_csv('FARMSIZE_with_rendements.csv', index=False)
 
-# Si vous souhaitez écraser le fichier existant, utilisez :
-df.to_csv('FARMSIZE_with_rendements.csv', index=False)
+
+print(df_normalized)
