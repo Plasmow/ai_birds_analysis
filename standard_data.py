1a4df22fd74766c2199ef2d47fe38b9c8aa96a63
Debbah Adam <adam.debbah@student-cs.fr>
2025-03-11 13:45:15 +0100

    function for all data (not tested)

--- standard_data.py
+++ standard_data.py
@@ -1,22 +1,32 @@
 import pandas as pd
 import numpy as np
+import os
 
-# Charger le fichier CSV en spécifiant le bon séparateur (point-virgule)
-file_path = "Pressure data/FARMSIZE.csv"
-df = pd.read_csv(file_path, sep=";", decimal=",", index_col=0)
+file_path_example = "Pressure data/FARMSIZE.csv"
+data={}
 
-
-
-df = df.apply(pd.to_numeric, errors='coerce')
-
-# Calcul du rendement pour chaque pays en suivant la formule donnée
-# Utilisation de shift(1) pour obtenir les valeurs de la colonne suivante (t+1)
-df_normalized = (df.shift(-1) - df) / df
-
-# Remplacement des valeurs extrêmes par NaN (par exemple, valeurs infinies ou NaN obtenues dans le calcul)
-df_normalized = df_normalized.replace([np.inf, -np.inf], np.nan)
+def load_data(file_path):
+    df = pd.read_csv(file_path, sep=";", decimal=",", index_col=0)
 
 
 
+    df = df.apply(pd.to_numeric, errors='coerce')
 
-print(df_normalized)
+    # Calcul du rendement pour chaque pays en suivant la formule donnée
+    # Utilisation de shift(1) pour obtenir les valeurs de la colonne suivante (t+1)
+    df_normalized = (df.shift(-1) - df) / df
+
+    # Remplacement des valeurs extrêmes par NaN (par exemple, valeurs infinies ou NaN obtenues dans le calcul)
+    df_normalized = df_normalized.replace([np.inf, -np.inf], np.nan)
+    
+    
+directory='Pressure data'
+
+def get_all_data(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            data[file]= os.path.join(root, file)
+
+get_all_data(directory)
+
+print(data['PLA.csv'])
